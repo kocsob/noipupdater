aba9f2a7ea9077840a16711ba22f1f6d83e7e351
Balazs Kocso <kocsob@users.noreply.github.com>
2015-06-29 00:04:03 +0200

    Command line arguments added

--- README.md
+++ README.md
@@ -6,20 +6,23 @@ Python script which uses the no-ip.com API to update the IP.
 Usage
 -----
 
-Configure the script with
-* correct API key (`username:password` encoded with base64),
-* hostname,
-* stored IP file (current IP) and
-* log file, then
-
-run it (`python noipupdater.py`).
+    noipupdater.py [-h] --api_key FILE --hostname NAME
+               [--stored_ip_file FILE] [--log_file FILE] [--log_level LEVEL]
+    
+    optional arguments:
+      -h, --help            show this help message and exit
+      --api_key FILE        file's path, which contains base64 encoded 'username:password'
+      --hostname NAME       NoIP hostname
+      --stored_ip_file FILE stored ip file path (default: /dev/shm/current_ip)
+      --log_file FILE       log file path (default: /var/log/noipupd.log)
+      --log_level LEVEL     logging level (default: INFO)
 
 For general use
 ---------------
 
 Place this in your cron file:
 
-    */15 * * * * python /file/location/noipupdater.py
+    */15 * * * * /file/location/noipupdater.py --api_key api.key --hostname hostname
 
 This will update your IP every fifteen minutes.
 
--- noipupdater.py
+++ noipupdater.py
@@ -1,7 +1,7 @@
-# !/usr/bin/python
+#!/usr/bin/python
 # -*- coding: utf8 -*-
 
-__version__ = '1.0'
+__version__ = '1.1'
 __author__ = 'Balazs Kocso'
 
 """ NoIPUpdater
@@ -9,22 +9,13 @@ __author__ = 'Balazs Kocso'
 Python script which uses the no-ip.com API to update the IP.
 """
 
-API_KEY = "api_key"    # base64.b64encode("username:password")
-HOSTNAME = "hostname"
-STORED_IP_FILE = "/dev/shm/current_ip"
-LOG_FILE = "/var/log/noipupd.log"
-
+import argparse
 import urllib
 import urllib2
 import logging
 import logging.handlers
 
 logger = logging.getLogger("NoIPUpdater")
-logger.setLevel(logging.INFO)
-handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=10 * 1024 ** 2, backupCount=5)
-fmt = logging.Formatter('[%(levelname)0.1s %(asctime)s %(name)s] %(message)s', datefmt="%y%m%d %H:%M:%S")
-handler.setFormatter(fmt)
-logger.addHandler(handler)
 
 IP_IDENTIFIERS = [
     "http://ipv4.icanhazip.com/",
@@ -52,7 +43,7 @@ def main():
         return
 
     try:
-        with open(STORED_IP_FILE, 'r') as fp:
+        with open(args.stored_ip_file, 'r') as fp:
             stored_ip = fp.read()
     except IOError:
         stored_ip = None
@@ -62,12 +53,19 @@ def main():
         return
 
     try:
+        with open(args.api_key, 'r') as fp:
+            api_key = fp.read().strip()
+    except (OSError, IOError) as e:
+        logger.warning(e.message)
+        return
+
+    try:
         get_params = urllib.urlencode({
-            "hostname": HOSTNAME,
+            "hostname": args.hostname,
             "myip": current_ip
         })
         request = urllib2.Request("https://dynupdate.no-ip.com/nic/update?%s" % (get_params))
-        request.add_header("Authorization", "Basic " + API_KEY)
+        request.add_header("Authorization", "Basic " + api_key)
         request.add_header("User-Agent", "Python No-IP Updater %s by %s" % (__version__, __author__))
         f = urllib2.urlopen(request)
         response = f.read()
@@ -86,10 +84,32 @@ def main():
         logger.warning(err.reason)
         return
 
-    with open(STORED_IP_FILE, 'w') as fp:
+    with open(args.stored_ip_file, 'w') as fp:
         fp.write(current_ip)
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="NoIP updater script.")
+    parser.add_argument("--api_key", metavar="FILE", required=True,
+        help="file's path, which contains the base64 encoded 'username:password'")
+    parser.add_argument("--hostname", metavar="NAME", required=True,
+        help="NoIP hostname")
+    parser.add_argument("--stored_ip_file", metavar="FILE", default="/dev/shm/current_ip",
+        help="stored ip file path (default: /dev/shm/current_ip)")
+    parser.add_argument("--log_file", metavar="FILE", default="/var/log/noipupd.log",
+        help="log file path (default: /var/log/noipupd.log)")
+    parser.add_argument("--log_level", metavar="LEVEL", default="INFO",
+        help="logging level (default: INFO)")
+    args = parser.parse_args()
+
+    handler = logging.handlers.RotatingFileHandler(args.log_file, maxBytes=10 * 1024 ** 2, backupCount=5)
+    fmt = logging.Formatter('[%(levelname)0.1s %(asctime)s %(name)s] %(message)s', datefmt="%y%m%d %H:%M:%S")
+    handler.setFormatter(fmt)
+    logger.addHandler(handler)
+    numeric_loglevel = getattr(logging, args.log_level.upper(), None)
+    if not isinstance(numeric_loglevel, int):
+        raise ValueError('Invalid log level: %s' % args.loglevel)
+    logger.setLevel(numeric_loglevel)
+
     try:
         main()
     except Exception as e:
